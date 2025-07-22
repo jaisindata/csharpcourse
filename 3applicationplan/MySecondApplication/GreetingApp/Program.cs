5cf975d9c910ccc9774b2ce241e3d857d6e862e2
jaisinoncloud <53889173+devtoprod-jsm@users.noreply.github.com>
2025-07-22 10:49:25 +0100

    Build an application with steps as comments (plan)

---
+++ 3applicationplan/MySecondApplication/GreetingApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GreetingApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Welcome user to app
+        Console.WriteLine("Welcome to the greeting app");
+        Console.WriteLine("----------------------------");
+        Console.WriteLine();
+
+        //Ask user for name
+        Console.Write("What is your name?");
+        string firstName;
+        firstName = Console.ReadLine();
+
+        //Greet user by name and end the application with greeting
+        Console.WriteLine($"Hello {firstName}");
+        Console.WriteLine("Thanks for using the app!");
+        Console.ReadLine();
+    }
+}
+
