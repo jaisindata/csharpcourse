a0eca572984bac0f0fb04f5a471b29133dbf75b8
jaisinoncloud <53889173+devtoprod-jsm@users.noreply.github.com>
2025-07-22 09:14:56 +0100

    Build a basic console app

---
+++ 2projectsandsolutions/MyFirstApplication/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloWorld;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //Console.WriteLine(args[0]);
+        Console.ReadLine();
+    }
+}
+
